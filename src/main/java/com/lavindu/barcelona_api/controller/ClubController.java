7ed75ca371cbc8af5383c11d0dcb38bb772114fe
LavinduWelikala <lavinduwelikala@gmail.com>
2025-01-12 20:57:08 +0530

    getClubs

--- src/main/java/com/lavindu/barcelona_api/controller/ClubController.java
+++ src/main/java/com/lavindu/barcelona_api/controller/ClubController.java
@@ -3,11 +3,15 @@ package com.lavindu.barcelona_api.controller;
 
 import com.lavindu.barcelona_api.controller.request.CreateClubDTO;
 import com.lavindu.barcelona_api.exception.ClubAlreadyExistException;
+import com.lavindu.barcelona_api.model.Club;
 import com.lavindu.barcelona_api.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ClubController {
@@ -18,6 +22,11 @@ public class ClubController {
     @PostMapping("/clubs")
     public void create(@RequestBody CreateClubDTO dto) throws ClubAlreadyExistException {
         clubService.createPlayer(dto);
+    }
+
+    @GetMapping("/clubs")
+    public List<Club> getAllClubs() {
+        return clubService.getAllClubs();
     }
 
 }
--- src/main/java/com/lavindu/barcelona_api/service/impl/ClubServiceImpl.java
+++ src/main/java/com/lavindu/barcelona_api/service/impl/ClubServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,6 +40,11 @@ public class ClubServiceImpl implements ClubService {
             clubRepository.save(club);
 
         }
+    }
+
+    @Override
+    public List<Club> getAllClubs() {
+        return clubRepository.findAll();
     }
 }
 

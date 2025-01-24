1283a4283d7cd29ab5239e358f4b012e40d0bd1a
LavinduWelikala <lavinduwelikala@gmail.com>
2025-01-24 22:52:15 +0530

    minor changes

--- src/main/java/com/lavindu/barcelona_api/model/Club.java
+++ src/main/java/com/lavindu/barcelona_api/model/Club.java
@@ -13,7 +13,7 @@ public class Club {
 
 
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
         private String name;
--- src/main/java/com/lavindu/barcelona_api/model/Match.java
+++ src/main/java/com/lavindu/barcelona_api/model/Match.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class Match {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private LocalDate date;

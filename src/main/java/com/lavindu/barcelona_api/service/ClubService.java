04eca8b1938907ee566861cdebf03dd2a373c848
LavinduWelikala <lavinduwelikala@gmail.com>
2025-02-02 16:19:26 +0530

    club getbyId added

--- src/main/java/com/lavindu/barcelona_api/service/ClubService.java
+++ src/main/java/com/lavindu/barcelona_api/service/ClubService.java
@@ -2,14 +2,17 @@ package com.lavindu.barcelona_api.service;
 
 import com.lavindu.barcelona_api.controller.request.CreateClubDTO;
 import com.lavindu.barcelona_api.exception.AlreadyExistException;
+import com.lavindu.barcelona_api.exception.ClubNotFoundException;
 import com.lavindu.barcelona_api.model.Club;
 
 import java.util.List;
 
 public interface ClubService {
 
-    void createPlayer(CreateClubDTO clubDTO) throws AlreadyExistException;
+    Club create(CreateClubDTO clubDTO) throws AlreadyExistException;
 
     List<Club> getAllClubs();
+
+    Club findById(Long clubId) throws ClubNotFoundException;
 
 }

0a63a31d668c570117c5c43799db01d16b73702f
LavinduWelikala <lavinduwelikala@gmail.com>
2025-04-27 16:09:05 +0530

    wrapper class added to the response of the controller

--- src/main/java/com/lavindu/barcelona_api/controller/response/ClubResponse.java
+++ src/main/java/com/lavindu/barcelona_api/controller/response/ClubResponse.java
@@ -1,8 +1,12 @@
 package com.lavindu.barcelona_api.controller.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClubResponse {
 
     private Long id;
